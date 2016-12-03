dedb0614e31c849f47c89e011e75801e1e2ac396
Hugo <hugo.lecomte@etu.univ-rouen.fr>
2016-12-03 12:04:22 +0100

    Ajout d' un test complémentaire sur l'envoie de donnée à une URL distante (communication avec une serveur)  (Java Rest Json)# Please enter the commit message for your changes. Lines starting

--- BoutsDeCode/DameRESTJava/src/main/java/config/AppConfig.java
+++ BoutsDeCode/DameRESTJava/src/main/java/config/AppConfig.java
@@ -4,10 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"controller"})
 public class AppConfig extends WebMvcConfigurerAdapter {
- 
+
 }
