214c13ce8de98a8c70530f53f8d717c6f4e24ebb
zeddycus <yohann.henry@etu.univ-rouen.fr>
2016-12-04 14:42:17 +0100

    pull new Rules for RandomPlayer

--- iataaaServer/src/main/java/fr/univ/iataaaserver/service/game/player/Player.java
+++ iataaaServer/src/main/java/fr/univ/iataaaserver/service/game/player/Player.java
@@ -1,18 +1,13 @@
 package fr.univ.iataaaserver.service.game.player;
 
+import fr.univ.iataaaserver.domain.game.*;
 import fr.univ.iataaaserver.service.game.exception.BusyException;
-import fr.univ.iataaaserver.domain.game.Difficulty;
-import fr.univ.iataaaserver.domain.game.EndGameCase;
-import fr.univ.iataaaserver.domain.game.EnumPlayer;
-import fr.univ.iataaaserver.domain.game.StatusService;
-import fr.univ.iataaaserver.domain.game.Token;
-import fr.univ.iataaaserver.service.game.game.GameImpl;
 
 public interface Player {
 
-    StatusService getStatut();
+    StatusService getStatus();
     void startGame(Token idGame, Difficulty difficulty) throws BusyException;
-    void PlayGame(Difficulty difficulty, GameImpl boardGame, EnumPlayer player) throws Exception;
+    Board<Case> PlayGame(Difficulty difficulty, Board<Case> boardGame, EnumPlayer player) throws Exception;
     void endGame(Token idGame, EndGameCase endType) throws Exception;
 
 }
---
+++ iataaaServer/src/main/java/fr/univ/iataaaserver/service/game/player/RandomizeCPUPlayer.java
@@ -0,0 +1,35 @@
+package fr.univ.iataaaserver.service.game.player;
+
+import fr.univ.iataaaserver.domain.game.*;
+import fr.univ.iataaaserver.service.game.exception.BusyException;
+import fr.univ.iataaaserver.service.game.util.Rules;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by z3ddycus on 04/12/16.
+ */
+public class RandomizeCPUPlayer implements Player {
+
+    @Override
+    public StatusService getStatus() {
+        return StatusService.AVAILABLE;
+    }
+
+    @Override
+    public void startGame(Token idGame, Difficulty difficulty) throws BusyException {
+
+    }
+
+    @Override
+    public Board<Case> PlayGame(Difficulty difficulty, Board<Case> boardGame, EnumPlayer player) throws Exception {
+        List<Board<Case>> coupsPossibles = Rules.getAvailableMoves(boardGame, player);
+        return coupsPossibles.get(new Random().nextInt(coupsPossibles.size()));
+    }
+
+    @Override
+    public void endGame(Token idGame, EndGameCase endType) throws Exception {
+
+    }
+}
