66b1db78bd5be9996528249d74af7af328a8ba8f
najwajmaa <jemaa.najwa@gmail.com>
2016-12-22 17:41:20 +0100

    start Game

--- src/main/java/fr/univrouen/iataaaserver/controller/GameController.java
+++ src/main/java/fr/univrouen/iataaaserver/controller/GameController.java
@@ -4,6 +4,7 @@ import fr.univrouen.iataaaserver.entities.Board;
 import fr.univrouen.iataaaserver.entities.Case;
 import fr.univrouen.iataaaserver.entities.Difficulty;
 import fr.univrouen.iataaaserver.entities.EnumPlayer;
+import fr.univrouen.iataaaserver.entities.PlayerType;
 import fr.univrouen.iataaaserver.entities.bean.GameBean;
 import fr.univrouen.iataaaserver.entities.status.StatusResponse;
 import fr.univrouen.iataaaserver.entities.bean.PlayerBean;
@@ -27,6 +28,33 @@ public class GameController {
 
     @Autowired
     private GamesService gamesService;
+    
+    // A supprimer
+    private GameController() {
+        PlayerBean p1 = new PlayerBean();
+        p1.setIp("127.0.0.1");
+        p1.setPort(8080);
+        p1.setName("ia1");
+        p1.setToken("toto");
+        p1.setDifficulty(Difficulty.MEDIUM);
+        p1.setType(PlayerType.IA);
+        
+        PlayerBean p2 = new PlayerBean();
+        p2.setIp("127.0.0.1");
+        p2.setPort(8080);
+        p2.setName("ia2");
+        p2.setToken("tata");
+        p2.setDifficulty(Difficulty.MEDIUM);
+        p2.setType(PlayerType.IA);
+        
+        gamesService.subscribePlayer(p1);
+        gamesService.subscribePlayer(p2);
+        
+        GameBean g = new GameBean();
+        g.setGameID("gameOne");
+        String[] players = new String[]{"ia1", "ia2"};
+        gamesService.createGame(g);
+    }
 
     @RequestMapping(value = { "/" }, method = RequestMethod.GET)
     public ResponseEntity<Set<String>> getGameNames(ModelMap model) {
